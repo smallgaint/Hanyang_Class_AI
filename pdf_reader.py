9886efb830275a7364881477d1893fba6b5ded0d
smallgaint <smallgaint@hanyang.ac.kr>
2026-04-04 11:40:55 +0900

    move all files

---
+++ pdf_reader.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+with open('Assignment1.pdf', 'rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    text = ''
+    for page in reader.pages:
+        text += page.extract_text()
+
+print(text)
